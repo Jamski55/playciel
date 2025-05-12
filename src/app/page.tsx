fa7c4e67a76dad497437022c89a760a1832b815e
Kalinowski James <james.kalinowski.2005@gmail.com>
2025-05-12 22:50:52 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-green-800 text-white">
+      <h1 className="text-4xl font-bold">Tailwind fonctionne 🎉</h1>
+    </main>
+  );
+}
